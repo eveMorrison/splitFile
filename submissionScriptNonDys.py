67270e6683526daab7156f1e19d62786c3c409c1
Eva Morrison <eva.morrison33@ku.edu>
2021-06-22 11:09:52 -0500

    create nondysgenic submission script creator

---
+++ submissionScriptNonDys.py
@@ -0,0 +1,25 @@
+interval = 60000
+lineno=0
+smallfile = None
+while (lineno<=6726270):
+    lineno = lineno + interval
+    if smallfile:
+        smallfile.close()
+    small_filename = 'dys_{}.sh'.format(lineno)
+    smallfile = open(small_filename, "w")
+    smallfile.write("#!/bin/bash\n")
+    smallfile.write("#SBATCH --job-name=repmask_dys"+str(lineno)+"\n")
+    smallfile.write("#SBATCH --partition=sixhour\n")
+    smallfile.write("#SBATCH --mail-type=ALL\n")
+    smallfile.write("#SBATCH --mail-user=e378m007@ku.edu\n")
+    smallfile.write("#SBATCH --time=0-06:00:00\n")
+    smallfile.write("#SBATCH --output=repmask_dys"+str(lineno)+"%j.log\n")
+    smallfile.write("#SBATCH --ntasks=1\n")
+    smallfile.write("#SBATCH --cpus-per-task=16\n")
+    smallfile.write("#SBATCH --mem=16gb\n")
+    smallfile.write("\n")
+    smallfile.write("module load repeatmasker/4.1.2\n")
+    smallfile.write("\n")
+    smallfile.write("echo \"running repeat masker\"\n")
+    smallfile.write("\n")
+    smallfile.write("RepeatMasker\n")
