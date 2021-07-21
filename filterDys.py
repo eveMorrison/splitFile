32992bd30a4a7c8f51453041e35b8b7accb7dded
Eva Morrison <eva.morrison33@ku.edu>
2021-07-21 10:58:45 -0500

    add more iterative command lines

--- filterDys.py
+++ filterDys.py
@@ -1,5 +1,7 @@
 interval = 60000
-lineno = 4800000
-while (lineno<=6720000):
+lineno = 120000
+while (lineno<=540000):
     lineno = lineno + interval
-    print("awk '{if (($7-$6) >= 250) print $0;}' dys_"+str(lineno)+".fa.out | awk '{if($2<=20.0) print $0;}' > dys_"+str(lineno)+"_filter.fa.out")
+    #print("awk '{if (($7-$6) >= 250) print $0;}' dys_"+str(lineno)+".fa.out | awk '{if($2<=20.0) print $0;}' > dys_"+str(lineno)+"_filter.fa.out")
+    #print("perl build_dictionary.pl --rm /home/e378m007/dysgenic/repMaskOut/dys_" + str(lineno) + "_filter.fa.out --unknown > dys" + str(lineno) + "_dict.txt")
+    print("perl one_code_to_find_them_all.pl --rm /home/e378m007/dysgenic/repMaskOut/dys_"+ str(lineno) +"_filter.fa.out --ltr /home/e378m007/dys"+ str(lineno) +"_dict.txt --strict --unknown --fasta /home/e378m007/dysgenic/dys_" + str(lineno) + ".fa --flanking 500") 
